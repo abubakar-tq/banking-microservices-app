12ab0171b6a52f1044f6004b983b9fe965b70bec
Abubakar <rabubakar351@gmail.com>
2026-05-13 13:30:00 +0500

    feat: expose auth, user, role and password REST controllers

---
+++ authentication-service/src/main/java/com/abubakar/authenticationservice/web/AuthenticationRestController.java
@@ -0,0 +1,25 @@
+package com.abubakar.authenticationservice.web;
+
+import com.abubakar.authenticationservice.dto.LoginRequestDTO;
+import com.abubakar.authenticationservice.dto.LoginResponseDTO;
+import com.abubakar.authenticationservice.service.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authentication")
+public class AuthenticationRestController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationRestController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO authenticate(@RequestBody LoginRequestDTO requestDTO) {
+        return authenticationService.authenticate(requestDTO);
+    }
+}
---
+++ authentication-service/src/main/java/com/abubakar/authenticationservice/web/NotificationRestClient.java
@@ -0,0 +1,13 @@
+package com.abubakar.authenticationservice.web;
+
+import com.abubakar.authenticationservice.dto.NotificationRequestDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "NOTIFICATION-SERVICE")
+public interface NotificationRestClient {
+
+    @PostMapping("/bank/notifications/send")
+    void sendNotification(@RequestBody NotificationRequestDTO request);
+}
